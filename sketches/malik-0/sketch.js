17a47fec3cae63be5fefd388529f3d1407905fd7
Kais Malik Ahmed <malik.ahmed@ecal.ch>
2025-12-04 13:59:01 +0100

    json changes

--- sketches/malik-0/sketch.js
+++ sketches/malik-0/sketch.js
@@ -1,54 +1,63 @@
-import { createEngine } from "../_shared/engine.js"
-import { createSpringSettings, Spring } from "../_shared/spring.js"
+import { createEngine } from "../_shared/engine.js";
+import { createSpringSettings, Spring } from "../_shared/spring.js";
 
-const { renderer, input, math, run, finish, } = createEngine()
-const { ctx, canvas } = renderer
-run(update)
+const { renderer, input, math, run, finish } = createEngine();
+const { ctx, canvas } = renderer;
+run(update);
 
-const spring = new Spring({
-  position: 0
-})
+const stars = [];
+const starCount = 5;
+const starSize = canvas.height * 0.25; // 10% of canvas height
+const starY = canvas.height / 2; // Center vertical position
 
-const settings1 = createSpringSettings({
-  frequency: 3.5,
-  halfLife: 0.05
-})
-const settings2 = createSpringSettings({
-  frequency: .2,
-  halfLife: 1.15
-})
+// Create 5 stars equally spaced horizontally
+for (let i = 0; i < starCount; i++) {
+  const spacing = canvas.width / (starCount + 1);
+  const star = {
+    x: spacing * (i + 1),
+    y: starY,
+    size: starSize,
+    velocity: 0,
+    acceleration: 0,
+    clicked: false,
+    rotation: 0,
+    alpha: (2 * Math.PI) / 10,
+    radius: starSize / 2,
+    starXY: [spacing * (i + 1), starY],
+  };
+  stars.push(star);
+}
 
+function drawStar(star) {
+  console.log("Drawing star at:", star.x, star.y);
+  ctx.save();
+  ctx.translate(star.x, star.y);
+  ctx.rotate(star.rotation);
+
+  ctx.beginPath();
+  for (let i = 11; i != 0; i--) {
+    const r = (star.radius * ((i % 2) + 1)) / 2;
+    const omega = star.alpha * i;
+    ctx.lineTo(r * Math.sin(omega), r * Math.cos(omega));
+  }
+  ctx.closePath();
+  ctx.strokeStyle = "#FFFFFF";
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  ctx.fillStyle = "#FFFFFF";
+  //ctx.fill();
+
+  ctx.restore();
+}
 
 function update(dt) {
+  console.log("mouse pos :", input.getX(), input.getY());
+  // Draw all stars
+  stars.forEach((star) => {
+    drawStar(star);
+  });
 
-  if (input.isPressed()) {
-    spring.target = -.1
-    spring.settings = settings2
+  if (stars.length <= 0) {
+    finish();
   }
-  else {
-    spring.target = 1
-    spring.settings = settings1
-  }
-
-  spring.step(dt)
-
-  const x = canvas.width / 2;
-  const y = canvas.height / 2;
-  const scale = Math.max(spring.position, 0)
-
-  ctx.fillStyle = "black"
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-  ctx.fillStyle = "white"
-  ctx.textBaseline = "middle"
-  ctx.font = `${canvas.height}px Helvetica Neue, Helvetica , bold`
-  ctx.textAlign = "center"
-  ctx.translate(x, y)
-  ctx.scale(scale, scale)
-  ctx.fillText("0", 0, 0)
-
-  if (scale <= 0) {
-    finish()
-  }
-
 }
